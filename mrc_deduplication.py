492c7d03883d02ea1f7edc14762a5da552abe4ee
Dan Riggins <daniel.riggins@protonmail.com>
2021-09-15 20:41:15 -0500

    Building model to predict person duplicates in python

--- mrc_deduplication.py
+++ mrc_deduplication.py
@@ -1,9 +1,23 @@
 import pandas as pd
 import pandas_dedupe as dd
+import sqlalchemy as sa
+import os
 
-deaths = pd.read_excel("data/MatchingtoMEdata.xlsx")
+os.chdir('/home/riggins/mrc_analyses/mrc_data')
 
-deaths_deduped = dd.dedupe_dataframe(
-    deaths,
-    ['DECEDENT_FIRST_NAME', 'DECEDENT_LAST_NAME', 'DECEDENT_DOB', 'DEATH_DATE']
+engine = sa.create_engine("mysql+pymysql://root@127.0.0.1:3306/mrc_data")
+
+people = pd.read_sql(
+        '''    
+        SELECT * FROM people
+        WHERE possible_dupe = 1
+        ''', engine)
+
+os.chdir('/home/riggins/mrc_analyses/mrc_data/people_model')
+
+deduped_people = dd.dedupe_dataframe(
+        people,
+        ['person_id', 'cmrn_id', 'rin_cc']
 )
+
+deduped_people.to_parquet('deduped_people')
